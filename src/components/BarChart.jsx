3babf24f1dbaa06248a368d5a5cc7e007ed7cc0c
Linxue <angelren1220@gmail.com>
2023-09-04 00:46:49 -0400

    add FetchData

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,39 +1,57 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import * as d3 from 'd3';
+import fetchData from '../hooks/fetchData';
 
 const BarChart = () => {
 
-  const barDataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+  const [values, setValues] = useState([]);
+  const [dates, setDates] = useState([]);
 
-  const w = 500;
-  const h = 150;
+  const dataLink = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+  const w = 800;
+  const h = 400;
 
   const svgRef = useRef(null);
 
   useEffect(() => {
+    fetchData(dataLink)
+      .then(data => {
+
+        const parsedDates = [];
+        const parsedValues = [];
+
+        const parseDate = d3.timeParse("%Y-%m-%d");
+
+        data.data.forEach(item => {
+          parsedDates.push(parseDate(item[0]));
+          parsedValues.push(item[1]);
+        });
+
+        setDates(parsedDates);
+        setValues(parsedValues);
+      })
+      .catch(error => console.error("Error fetching data:", error));
+
+  }, []);
+
+  useEffect(() => {
     const svg = d3.select(svgRef.current);
+
+    // Clear previous SVG content
+    svg.selectAll('*').remove();
+
     svg.selectAll("rect")
-      .data(barDataset)
+      .data(values)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 30)
-      .attr("y", (d) => h - d * 3)
-      .attr("width", 25)
-      .attr("height", (d) => 3 * d)
+      .attr("x", (d, i) => i * 3)
+      .attr("y", (d) => h - 0.02 * d)
+      .attr("width", 2)
+      .attr("height", (d) => 0.02 * d)
       .attr("fill", "navy")
       .attr("class", "bar")
-      .append("title")
-      .text((d) => (d));
-
-    svg.selectAll(".barLabel")
-      .data(barDataset)
-      .enter()
-      .append("text")
-      .attr("class", "barLabel")
-      .attr("x", (d, i) => i * 30)
-      .attr("y", (d) => h - d * 3 - 3)
-      .text((d) => (d));
-  }, [barDataset]);
+  }, [values]);
 
 
   return (

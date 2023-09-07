fe63348fcb3ca092aff227755db81029b93dca15
Linuxe <angelren1220@gmail.com>
2023-09-07 03:08:45 -0400

    add legends for heatmap

--- src/components/HeatMapChart.jsx
+++ src/components/HeatMapChart.jsx
@@ -23,7 +23,12 @@ const HeatMapChart = () => {
         const parsedData = [];
 
         data.monthlyVariance.forEach(item => {
-          parsedData.push(item);
+          parsedData.push({
+            year: item.year,
+            month: item.month,
+            temp: item.variance + baseTemperature,
+            variance: item.variance
+          });
         });
 
         console.log(parsedData);
@@ -52,8 +57,9 @@ const HeatMapChart = () => {
 
       // set color scale
       const colors = gradientColors();
-      
+
       const variance = data.map(d => d.variance);
+      const temp = data.map(d => d.temp);
       const colorScale = d3.scaleQuantize()
         .domain([d3.min(variance), d3.max(variance)])
         .range(colors);
@@ -76,17 +82,55 @@ const HeatMapChart = () => {
       const yAxis = d3.axisLeft(yScale)
         .tickFormat(d => monthNames[d]);
 
-
       svg.append("g")
         .attr("transform", `translate(0, ${height - margin.bottom})`)
         .call(xAxis);
+
+      svg.append("text")
+        .attr("transform", `translate(${width / 2}, ${height})`)
+        .style("text-anchor", "middle")
+        .text("Year");
 
       svg.append("g")
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(yAxis);
 
+      // draw legends
+      const legendWidth = 400;
+      const legendHeight = 20;
+
+      const legend = svg.append("g")
+        .attr("transform", `translate(${width - legendWidth}, ${height + 40})`);
+
+      const numRects = colors.length;
+      const rectWidth = legendWidth / numRects;
+
+      legend.selectAll("rect")
+        .data(colors)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * rectWidth)
+        .attr("y",  legendHeight - 30)
+        .attr("width", rectWidth)
+        .attr("height", legendHeight)
+        .attr("fill", d => d);
+
+      // add labels
+      const legendScale = d3.scaleLinear()
+        .domain([0, numRects - 1])
+        .range([d3.min(temp), d3.max(temp)]);
+
+        legend.selectAll("text")
+        .data(colors)
+        .enter()
+        .append("text")
+        .attr("x", (d, i) => i * rectWidth)
+        .attr("y", legendHeight + 10)
+        .text((d, i) => legendScale(i).toFixed(1))
+        .style("font-size", "15px")
+        .attr("text-anchor", "middle")
     }
-  }, [data]);
+  }, [data, height, margin.bottom, margin.left, margin.right, margin.top, width]);
 
 
   return (
